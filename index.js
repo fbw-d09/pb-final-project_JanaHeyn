dba4734dba01eecf7e2dd9b4d01644921f7bee11
JanaHeyn <jana.heyn@web.de>
2023-04-26 16:23:47 +0200

    Update monster game

--- index.js
+++ index.js
@@ -7,62 +7,185 @@ const Player = require('./src/Player');
 
 // Spieler
 const elliot = new Player("Elliot 🐉️", 120, 80);
-const freeky = new Player("Freeky 🧟", 120, 80);
+// const freeky = new Player("Freeky 🧟", 120, 80);
 const elfie = new Player("Elfie 🧝", 120, 80);
-const jeannie = new Player("Jeannie 🧞️", 120, 80)
+// const jeannie = new Player("Jeannie 🧞️", 120, 80);
 
 // Fähigkeiten
-const blitz = new AttackSkill("Blitz 🌩️ ", 50, 50);
-const einfrieren = new AttackSkill("Einfrieren 🧊️", 40, 30);
-const versteinerung = new AttackSkill("Versteinerung 🗿️", 40, 30);
-const feuer = new AttackSkill("Feuer 🔥️", 30, 20);
-const wasserfontäne = new AttackSkill("Wasserfontäne 🌊️", 40, 20)
-const giftpille = new AttackSkill("Giftpille 💊️", 20, 10);
+const blitz = new AttackSkill("Blitz 🌩️ ", 60, 50);
+const bombe = new AttackSkill("Bombe 💣️", 60, 50);
 
+const einfrieren = new AttackSkill("Einfrieren 🧊️", 40, 20);
+const versteinerung = new AttackSkill("Versteinerung 🗿️", 40, 20);
+
+const feuer = new AttackSkill("Feuer 🔥️", 20, 10);
+const wasserfontäne = new AttackSkill("Wasserfontäne 🌊️", 20, 10);
+
+
+// 1.Kampf | Runde 1
+/* console.log("1. Kampf | Elfie vs Jeannie | Runde 1:");
+console.log();
 console.log(elfie.getStatus());
 console.log();
 console.log(jeannie.getStatus());
+console.log();*/
+// console.log(elfie.learnSkill(bombe));
+// console.log(jeannie.learnSkill(blitz));
+/*console.log();
+console.log(elfie.getStatus());
 console.log();
-console.log(elfie.learnSkill(blitz));
-console.log();
-console.log(jeannie.learnSkill(versteinerung));
-console.log();
+console.log(jeannie.getStatus());
+console.log();*/
+// console.log(elfie.learnSkill(versteinerung));
+// console.log(jeannie.learnSkill(einfrieren));
+/*console.log();
 console.log(elfie.getStatus());
 console.log();
 console.log(jeannie.getStatus());
 console.log();
 console.log(elfie.attack(0, jeannie));
+console.log(elfie.getStatus());
+console.log();
+console.log(jeannie.getStatus());
+console.log();
+console.log(jeannie.attack(0,elfie));
+console.log(jeannie.getStatus());
+console.log();
+console.log(elfie.getStatus()); */
+
+// 1. Kampf | Runde 2
+// elfie kraft: 60, magie: 30, fähigkeiten: 
+// jeannie kraft:60, magie: 30
+/* console.clear()
+console.log("1. Kampf | Elfie vs Jeannie | Runde 2:")
+console.log();
+console.log(elfie.getStatus());
+console.log();
+console.log(jeannie.getStatus()); */
+// console.log(jeannie.getRandomItem(elfie));
+// jeannie: -300 Muscheln
+// elfie: -30 Kraftpunkte = 30 rest Kraft
+/* console.log();
+console.log(elfie.attack(1, jeannie));
+console.log(); */
+
+// 1. Kampf | Runde 3
+/* console.log("1. Kampf | Elfie vs Jeannie | Runde 3:")
 console.log();
 console.log(elfie.getStatus());
 console.log();
 console.log(jeannie.getStatus());
 console.log();
-console.log(jeannie.attack(0, elfie));
+console.log(elfie.getRandomItem(jeannie));
+console.log(); */
+//Gewinner 1.Kampf: Elfie
+
+
+
+// 2. Kampf | Runde 1
+/* console.clear();
+console.log("2. Kampf | Elliot vs Freeky | Runde 1:");
+console.log();
+console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+console.log(elliot.learnSkill(feuer));
+console.log();
+console.log(freeky.learnSkill(wasserfontäne));
+console.log();
+console.log(elliot.learnSkill(versteinerung));
+console.log();
+console.log(freeky.learnSkill(einfrieren));
+console.log();
+console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+console.log(elliot.attack(1, freeky));
+console.log();
+console.log(freeky.attack(0, elliot));
+console.log(); */
+
+// 2. Kampf | Runde 2
+/* console.log("2. Kampf | Elliot vs Freeky | Runde 2:");
+console.log();
+console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+
+console.log(elliot.learnSkill(blitz));
+console.log(); */
+// console.log(freeky.getRandomItem(elliot));
+// console.log();
+// freeky erhält eine wasserpistole:
+// zwischen-status freeky:
+// kraft: 80, magie: 70, händisch umändern --> muscheln: 600 -> 300, 
+/* console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+console.log(elliot.attack(2, freeky));
+console.log();
+console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+console.log(); */
+// console.log(freeky.getRandomItem(elliot));
+// freeky gibt 300 ab und erhält 600:
+// zwischen-status freeky
+// kraft: 20, magie: 70, händisch umändern --> msucheln: 300 -> 0;
+/* console.log(elliot.getStatus());
+console.log();
+console.log(freeky.getStatus());
+console.log();
+console.log(elliot.attack(0, freeky)); */
+//Gewinner 2.Kampf: Elliot
+
+// 3. Kampf 1. Runde
+/* console.log("3. Kampf | Elfie vs Elliot | Runde 1:");
 console.log();
 console.log(elfie.getStatus());
+console.log(elliot.getStatus());
+console.log(); */
+/* console.log(elfie.learnSkill(blitz));
 console.log();
-console.log(jeannie.getStatus());
+console.log(elliot.learnSkill(bombe));
 console.log();
-console.log(jeannie.learnSkill(einfrieren));
+console.log(elfie.learnSkill(versteinerung));
 console.log();
-console.log(jeannie.getStatus());
-console.log();
-console.log(jeannie.attack(1, elfie));
-console.log();
-console.log(jeannie.getStatus());
-console.log();
-console.log(elfie.getStatus());
-console.log();
-console.log(jeannie.learnSkill(wasserfontäne));
-console.log();
-console.log(jeannie.getStatus());
-console.log();
-console.log(jeannie.attack(2, elfie));
-console.log();
-console.log(jeannie.getStatus());
+console.log(elliot.learnSkill(einfrieren));
+console.log(); */
+/* console.log(elfie.getStatus());
+console.log(elliot.getStatus()); */
+// console.log();
+// console.log(elfie.attack(0, elliot));
+// console.log();
+// console.log(elliot.attack(0, elfie));
+// 3. Kampf | Runde 2
+/* console.log("3. Kampf | Elfie vs Elliot | Runde 2:");
 console.log();
 console.log(elfie.getStatus());
-console.log();
-console.log(elfie.attack(0, jeannie));
+console.log(elliot.getStatus());
+console.log(); */
+// console.log(elfie.attack(0, elliot));
+// console.log(blitz);
+/* console.log();
+console.log(elfie.getMagic());
+console.log(); */
+/* console.log(elfie.getStatus());*/
+/* console.log(elliot.getStatus()); 
+console.log(bombe);
+console.log(einfrieren);
+console.log(); */
+// console.log(elliot.attack(1, elfie));
+/* console.log();
+console.log(elfie.getStatus()); */
+console.log(elliot.getStatus());
 console.log();
 console.log(blitz);
+console.log(versteinerung);
+console.log();
+console.log(elfie.attack(0, elliot));
--- src/Player.js
+++ src/Player.js
@@ -1,17 +1,17 @@
 class Player
 {
     name;
-    health;
+    power;
     magic;
     skills;
     money;
 
-    constructor(name, health, magic) {
+    constructor(name, power, magic, money = 600) {
         this.name = name;       // string + symbol  
-        this.health = health;   // nummer
+        this.power = power;     // nummer
         this.magic = magic;     // nummer
         this.skills = [];       // wird erlernt, 
-        this.money = 600;       // immer vorgegegeben 
+        this.money = money;       // Defaultwert 600 
     }
 
     getSkills() {
@@ -33,12 +33,12 @@ class Player
         // console.log("name:", skillNames);
         
         if(this.skills.length == 0) {
-            return `--- Status von ${this.name} ---\r\nGesundheit 💖️ \t: ${this.health}\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: noch keine Fähigkeiten erlernt\r\nGeld 🐚️ \t: ${this.money}`
+            return `--- Status von ${this.name} ---\r\nKraft 💖️ \t: ${this.power}\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: noch keine Fähigkeiten erlernt\r\nGeld 🐚️ \t: ${this.money}`
         }
         else if(this.health == 0) {
-            return `--- Status von ${this.name} ---\r\nGesundheit 💖️ \t: 💀️\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: ${skillNames}\r\nGeld 🐚️ \t: ${this.money}`
+            return `--- Status von ${this.name} ---\r\nKraft 💖️ \t: 💀️\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: ${skillNames}\r\nGeld 🐚️ \t: ${this.money}`
         }
-        return `--- Status von ${this.name} ---\r\nGesundheit 💖️ \t: ${this.health}\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: ${skillNames}\r\nGeld 🐚️ \t: ${this.money}`
+        return `--- Status von ${this.name} ---\r\nKraft 💖️ \t: ${this.power}\r\nMagie 🪄️ \t: ${this.magic}\r\nFähigkeiten 💪️ \t: ${skillNames}\r\nGeld 🐚️ \t: ${this.money}`
     }
 
     // Methode: Angreifen
@@ -50,7 +50,7 @@ class Player
         // console.log("das ist die attacke:", attack);
 
         // prüfen, ob genug Gesundheit
-        if(this.health <= 0) {
+        if(this.power <= 0) {
             return `🪦️  ${this.name} ist bereits ausgeschieden und kann nicht mehr kämpfen.`;   
         } 
         // prüfen, ob genug Magie 
@@ -58,65 +58,82 @@ class Player
             return `⛔️ ${this.name} hat nicht genug Magie für diesen Angriff.`;
         }
         // prüfen, ob Gegner nach Abzug des Schadens <= 0 bzw dann tot ist
-        else if(enemy.health - attack.damage <= 0) {
-            enemy.health = 0;
-            return `${this.name} hat gewonnen! Alle Gesundheitspunkte von ${enemy.name} 💀️ aufgebraucht.`
+        else if(enemy.power - attack.damage <= 0) {
+            enemy.power = 0;
+            return `${enemy.name} hat keine Kraft mehr und gibt auf 🏳️.\r\n${this.name} hat gewonnen 🏆️!`
         }
         // Abzug damage von Gegner
         else {
-            enemy.health -= attack.damage;
+            enemy.power -= attack.damage;
             this.magic -= attack.neededmagic;
-            return `💥️ ${this.name} hat die Fähigkeit ${attack.name} erfolgreich ausgeführt!\r\n- minus ${attack.neededmagic} Magiepunkte für ${this.name}\r\n- minus ${attack.damage} Gesundheitspunkte für ${enemy.name}.`    
+            return `💥️ ${this.name} hat die Fähigkeit ${attack.name} erfolgreich ausgeführt!\r\n- minus ${attack.damage} Kraftpunkte für ${enemy.name}\r\n- minus ${attack.neededmagic} Magiepunkte für ${this.name}.`    
         }
     }
 
     getMagic() {
-        // random mäßig bekommt der Spieler etwas Magie zurück von Merlin
+        // der Spieler etwas Magie zurück von Merlin
+        this.magic += 20; 
 
-        // vorgegebenes Array mit möglichen Werten
-        const addMagic = [10, 20, 30, 40, 50];
-
-        // random index vom Element
-        const random = Math.floor(Math.random()*addMagic.length);
-        // console.log(random);
-
-        // random element
-        // addMagic[random];
-        
-        // this.magic mit geschenkter Magie addieren
-        this.magic += addMagic[random]; 
-        return `🪄️  🪄️  🪄️  - ${this.name} hat ${addMagic[random]} Magiepunkte von Merlin 🧙️ erhalten!` + `\r\n${this.name} besitzt nun insgesamt ${this.magic} Magiepunkte.`;
+        return `🪄️  🪄️  🪄️  - ${this.name} hat 20 Magiepunkte von Merlin 🧙️ erhalten!` + `\r\n${this.name} besitzt nun insgesamt ${this.magic} Magiepunkte.`;
     }
 
     getRandomItem(enemy) {
-        // mit Muscheln eine Überraschung kaufen
-        const items = ["Gesundheit 💖️", "Magieklau 🧲️🪄️", "Angriff 🗡️", "Wasserpistole 🔫️"];
+        // mit Muscheln eine Überraschung kaufen aus diesem Array:
+        const items = ["Kraft 💖️", "Magieklau 🧲️🪄️", "Angriff 🗡️", "Wasserpistole 🔫️", "Geld 🐚️"];
 
+        // zufällig ein Item auswählen und in einer Variablen abspeichern:
         const random = Math.floor(Math.random()*items.length);
-
         const randomItem = items[random];
 
-        // geld wird abgezogen
+        // Muscheln werden abgezogen:
         this.money -= 300;
 
         // Konditionen erstellen für jedes Item:
-        if(randomItem === "Gesundheit 💖️") {
-            this.health += 30;
-            return `${this.name} hat für 300🐚️ ${randomItem} gekauft und erhält 30 Gesundheitspunkte!`
+        if(randomItem === "Kraft 💖️") {
+            this.power += 30;
+            return `${this.name} gibt 300🐚️ ab und erhält dafür +30 ${randomItem} Punkte!`
         }
         else if(randomItem === "Magieklau 🧲️🪄️") {
-            enemy.magic -= 30;
-            return `${this.name} hat für 300🐚️ ${randomItem}  gekauft und entzieht ${enemy.name} 30 Magiepunkte!`
+            if(enemy.magic === 0) {
+                return `Sorry! Bei ${enemy.name} gibt es keine Magiepunkte mehr zu klauen.`
+            }
+            else if(enemy.magic === 10) {
+                enemy.magic = 0;
+                this.magic += 10;
+                return `${this.name} hat für 300🐚️ ${randomItem} eingetauscht und klaut von ${enemy.name} die restlichen 10 Magiepunkte!`
+            }
+            else if(enemy.magic === 20) {
+                enemy.magic = 0;
+                this.magic += 20;
+                return `${this.name} hat für 300🐚️ ${randomItem}  eingetauscht und klaut von ${enemy.name} alle 20 Magiepunkte!`
+            }
+            else if(enemy.magic >= 30) {
+                enemy.magic -= 30;
+                this.magic += 30;
+                return `${this.name} hat für 300🐚️ ${randomItem}  eingetauscht und klaut von ${enemy.name} ganze 30 Magiepunkte!`
+            }
         }
         else if(randomItem === "Angriff 🗡️") {
-            enemy.health -= 30;
-            return `${this.name} hat für 300🐚️ ${randomItem}  gekauft und greift ${enemy.name} an. ${enemy.name} verliert 30 Gesundheitspunkte!`
+            if(enemy.power === 10 || enemy.power === 20 || enemy.power === 30) {
+                enemy.power = 0;
+                return `${this.name} hat für 300🐚️ einen Extrtazug ${randomItem}  gekauft und greift ${enemy.name} an.\r\n${enemy.name} 🏳️  gibt auf und verliert das Spiel.\r\n${this.name} gewinnt das Spiel 🏆️!`
+            }
+            else {
+                enemy.power -= 30;
+                return `${this.name} hat für 300🐚️ einen Extrtazug ${randomItem}  gekauft und greift ${enemy.name} an.\r\n${enemy.name} verliert 30 Kraftpunkte!`
+            }
+        }
+        else if(randomItem === "Geld 🐚️") {
+            this.money += 600;
+            return `${this.name} gibt ❌️ 300🐚️ ab und erhält ✅️ 600🐚️ zurück!`
         }
         else {
-            return `${this.name} hat eine ${randomItem} erhalten, das ist leider eine Niete, die 300🐚️ werden dennoch abgezogen 🤷️!`
+            return `${this.name} hat eine ${randomItem} erhalten 🎉️🎉️🎉️!\r\nAch so, das ist übrigens eine Niete 🎭️\r\nDie 300🐚️ werden dennoch abgezogen 🤷️!`;
+            
         }
 
         // console.log(items[random]);
+        // console.log(this.getStatus());
     }
 }
 
